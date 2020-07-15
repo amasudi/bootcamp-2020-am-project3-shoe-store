6087a1b40da5df9ca50ae37050d978eaf183e121
amasudi <a.masudi@gmail.com>
2020-07-15 19:38:14 +0500

    removing error from home component for filter records

--- src/components/CategoryIndex.js
+++ src/components/CategoryIndex.js
@@ -16,41 +16,44 @@ export const CategoryIndex = () => {
   let { data } = useContext(GlobalContext);
   const { catID } = useParams();
   return (
-    <Grid container spacing={2} style={{ padding: 10 }}>
-      {Object.entries(data[catID]).map(([menID, { title, rate, imgSrc }]) => {
-        return (
-          <Grid key={menID} item xs={12} sm={3}>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  alt={title}
-                  height="250"
-                  image={imgSrc}
-                  title={title}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {title}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    ${rate}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardActions>
-                <Button size="small" color="primary">
-                  Show Details
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+    <div>
+      <h1 style={{ textAlign: "center" }}>{catID.toUpperCase()}</h1>
+      <Grid container spacing={2} style={{ padding: 10 }}>
+        {Object.entries(data[catID]).map(([menID, { title, rate, imgSrc }]) => {
+          return (
+            <Grid key={menID} item xs={12} sm={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={title}
+                    height="250"
+                    image={imgSrc}
+                    title={title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      ${rate}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Show Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
   );
 };
