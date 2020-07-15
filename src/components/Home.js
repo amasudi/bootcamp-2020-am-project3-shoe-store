6486fe845aa696c201c209e53e60d5705fdc1044
amasudi <a.masudi@gmail.com>
2020-07-15 19:21:22 +0500

    removing error from home component for filter records

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,120 +17,114 @@ export const Home = () => {
   } = useContext(GlobalContext);
   return (
     <Grid container spacing={2} style={{ padding: 10 }}>
-      {Object.entries(men).map(
-        ([menID, { title, rate, imgSrc, showOnHome }]) => {
-          if (showOnHome) {
-            return (
-              <Grid key={menID} item xs={12} sm={3}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={title}
-                      height="250"
-                      image={imgSrc}
-                      title={title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        ${rate}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Show Details
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          }
-        }
-      )}
-      {Object.entries(women).map(
-        ([womenID, { title, rate, imgSrc, showOnHome }]) => {
-          if (showOnHome) {
-            return (
-              <Grid key={womenID} item xs={12} sm={3}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={title}
-                      height="250"
-                      image={imgSrc}
-                      title={title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        ${rate}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Show Details
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          }
-        }
-      )}
-      {Object.entries(kids).map(
-        ([kidID, { title, rate, imgSrc, showOnHome }]) => {
-          if (showOnHome) {
-            return (
-              <Grid key={kidID} item xs={12} sm={3}>
-                <Card>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt={title}
-                      height="250"
-                      image={imgSrc}
-                      title={title}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {title}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        ${rate}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      Show Details
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            );
-          }
-        }
-      )}
+      {Object.entries(men)
+        .filter(([menObj, { showOnHome }]) => showOnHome)
+        .map(([menID, { title, rate, imgSrc, showOnHome }]) => {
+          return (
+            <Grid key={menID} item xs={12} sm={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={title}
+                    height="250"
+                    image={imgSrc}
+                    title={title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      ${rate}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Show Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      {Object.entries(women)
+        .filter(([womenObj, { showOnHome }]) => showOnHome)
+        .map(([womenID, { title, rate, imgSrc, showOnHome }]) => {
+          return (
+            <Grid key={womenID} item xs={12} sm={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={title}
+                    height="250"
+                    image={imgSrc}
+                    title={title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      ${rate}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Show Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      {Object.entries(kids)
+        .filter(([kidObj, { showOnHome }]) => showOnHome)
+        .map(([kidID, { title, rate, imgSrc, showOnHome }]) => {
+          return (
+            <Grid key={kidID} item xs={12} sm={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt={title}
+                    height="250"
+                    image={imgSrc}
+                    title={title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {title}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      ${rate}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Show Details
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
     </Grid>
   );
 };
